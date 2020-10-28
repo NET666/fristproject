ec5fa1695ecfd910afbe465ed600cccac3859942
NET666 <1724940500@qq.com>
2020-10-28 10:48:21 +0800

    抓取豆瓣评论(评论者头像，昵称，发布时间，评论内容)

--- php/doubanComment.php
+++ php/doubanComment.php
@@ -6,13 +6,14 @@ $start = $_GET['start'];
 $id = $_GET['id'];
 //爬取豆瓣网评论
 if($start=='0'){
-	$arrayList = array(array('userName'=>'','commentTime'=>'','commentContent'=>''));
+	
 	$url = 'https://movie.douban.com/subject/'.$id.'/comments?limit=20&status=P&sort=new_score';
 	//$url = file_get_contents($url);
 	phpQuery::newDocumentFile($url);
-	$userNames = pq(".comment-info a");
-	$commentTimes = pq(".comment-time");
-	$commentContents = pq(".short");
+	$userNames = pq(".comment-info a");//昵称
+	$commentTimes = pq(".comment-time");//时间
+	$commentContents = pq(".short");//评论
+	$userImages = pq(".avatar img");//评论者用户头像
 	$i = 0;
 	foreach($userNames as $userName){
 		$arrayList[$i]['userName'] = pq($userName)->text();
@@ -27,6 +28,11 @@ if($start=='0'){
 	foreach($commentContents as $commentContent){
 		$arrayList[$l]['commentContent'] = pq($commentContent)->text();
 		$l++;
+	}
+	$n = 0;
+	foreach($userImages as $userImage){
+		$arrayList[$n]['userImage'] = pq($userImage)->attr('src');
+		$n++;
 	}
 	
 	//转换json的格式
@@ -34,13 +40,14 @@ if($start=='0'){
 }
 else{
 
-	$arrayList = array(array('userName'=>'','commentTime'=>'','commentContent'=>''));
+	$arrayList = array(array('userName'=>'','commentTime'=>'','commentContent'=>'','userImage'=>''));
 	$url = 'https://movie.douban.com/subject/'.$id.'/comments?start='.$start.'&limit=20&status=P&sort=new_score';
 	//$url = file_get_contents($url);
 	phpQuery::newDocumentFile($url);
-	$userNames = pq(".comment-info a");
-	$commentTimes = pq(".comment-time");
-	$commentContents = pq(".short");
+	$userNames = pq(".comment-info a");//昵称
+	$commentTimes = pq(".comment-time");//时间
+	$commentContents = pq(".short");//评论
+	$userImages = pq(".avatar img");//评论者用户头像
 	$i = 0;
 	foreach($userNames as $userName){
 		$arrayList[$i]['userName'] = pq($userName)->text();
@@ -55,6 +62,11 @@ else{
 	foreach($commentContents as $commentContent){
 		$arrayList[$l]['commentContent'] = pq($commentContent)->text();
 		$l++;
+	}
+	$n = 0;
+	foreach($userImages as $userImage){
+		$arrayList[$n]['userImage'] = pq($userImage)->attr('src');
+		$n++;
 	}
 	
 	//转换json的格式
--- php/getNavitationBar.php
+++ php/getNavitationBar.php
@@ -5,14 +5,15 @@
  * Date: 2020/10/21
  * Time: 20:24
  */
-
+//header('Content-Type: text/html; charset=UTF-8');
 header("content-type:application/json;charset=utf-8");
 include_once '../phpQuery/phpQuery/phpQuery.php';
 phpQuery::newDocumentFile('https://movie.douban.com/');
 
 $images = pq(".poster img"); //爬取.poster类名中的所有img标签（用于下面获取图片地址和影视名称）
 $getRate = pq(".rating .subject-rate");//爬取.rating类名里的所有 .subject-rate命名的标签
-$getImages =  array(array('image'=>'','title'=>'','rate'=>''));
+$getUrls = pq(".ui-slide-content .title a");
+$getImages =  array(array('image'=>'','title'=>'','rate'=>'','url'=>''));
 $i = 0;
 foreach ($images as $img){
 	$getImages[$i]['image'] = pq($img)->attr("src"); //获取对应标签里面的src属性值
@@ -23,6 +24,11 @@ $k = 0;
 foreach($getRate as $Rate){
 	$getImages[$k]['rate'] = pq($Rate)->text();
 	$k++;
+}
+$j = 0;
+foreach($getUrls as $getUrl){
+	$getImages[$j]['url'] = pq($getUrl)->attr("href");
+	$j++;
 }
 //转换json的格式
 echo json_encode($getImages);
